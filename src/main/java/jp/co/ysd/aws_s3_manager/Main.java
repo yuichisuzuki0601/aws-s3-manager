bc1b71bb8671a59817ea83e5cc22b294aa54b715
yuichi <ys.development0601@gmail.com>
2020-05-19 20:09:43 +0900

    アクセス情報クラスをインナークラス化

--- src/main/java/jp/co/ysd/aws_s3_manager/Main.java
+++ src/main/java/jp/co/ysd/aws_s3_manager/Main.java
@@ -1,10 +1,18 @@
 package jp.co.ysd.aws_s3_manager;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
@@ -14,6 +22,35 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
  *
  */
 public class Main {
+
+	private static class AccessInfo {
+		private String region;
+		private String accessKey;
+		private String secretKey;
+		private String bucketName;
+
+		public AccessInfo() throws IOException {
+			Properties properties = new Properties();
+			try (InputStream is = new FileInputStream("s3-access.properties")) {
+				properties.load(is);
+			}
+			this.region = properties.getProperty("region");
+			this.accessKey = properties.getProperty("accessKey");
+			this.secretKey = properties.getProperty("secretKey");
+			this.bucketName = properties.getProperty("bucketName");
+		}
+
+		private String getBucketName() {
+			return bucketName;
+		}
+
+		private AmazonS3 getS3Client() {
+			return AmazonS3ClientBuilder.standard()
+					.withRegion(Regions.fromName(region))
+					.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+					.build();
+		}
+	}
 
 	// java -jar aws-s3-manager-1.0.0.jar upload [uploadFilePath]
 	// java -jar aws-s3-manager-1.0.0.jar download [targetObjectName] [downloadDirectory]
